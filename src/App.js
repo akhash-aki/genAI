6cf49961be53f82cb6b779b2a3dc73fa346f4341
akhash-aki <162665694+akhash-aki@users.noreply.github.com>
2024-10-20 20:49:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
         // Send message to backend
         try {
-            const response = await fetch('https://alert-raven-wealthy.ngrok-free.app', {
+            const response = await fetch('https://alert-raven-wealthy.ngrok-free.app/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
